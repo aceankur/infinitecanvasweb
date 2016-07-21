a4e43f6b12bdd58f875261a216b5f7e8ccd4a86f
Ankur Jain <ankurjain.v@darthvader.local>
2016-07-22 00:21:15 +0530

    construct location packet for android

--- location.js
+++ location.js
@@ -1,12 +1,24 @@
 $(function() {
   if ("geolocation" in navigator) {
     var geo_options = {
-      enableHighAccuracy: true, 
-      maximumAge: 5000
+      enableHighAccuracy: true
     };
     var watchID = navigator.geolocation.watchPosition(function(position) {
       console.log(position);
-      Android.sendLocation(position.coords);
+      try {
+        if(position != null && position.coords != null)
+        Android.sendLocation({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          altitude: position.coords.altitude
+          accuracy: position.coords.accuracy,
+          altitudeAccuracy: position.coords.altitudeAccuracy,
+          heading: position.coords.heading,
+          speed: position.coords.speed
+        });
+      } catch(e) {
+        console.error(e);
+      }
     });
   }
 });
